3c874d96c968ef3bea64c00090bf04e190a43906
danielbarcheta <danielbarcheta@gmail.com>
2024-09-09 18:54:37 -0300

    ft Component ReservaForm

--- src/components/reservas/ReservaForm.jsx
+++ src/components/reservas/ReservaForm.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ReservaForm = () => {
+    const [isValidado, setIsValidado] = useState(false)
+    const [submited, isSubmited] = useState(false)]
+    const [mensagemErro, setMensagemErro] = useState("")
+    const [precoQuarto, setPrecoQuarto] = useState(0)
+    const [reserva, setReserva] = useState({})
   return (
     <div>ReservaForm</div>
   )
